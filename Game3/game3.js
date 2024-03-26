03eeb41e5951b53af6f786ff046e54982fd3992a
Carsynschuch <cschuch@asu.edu>
2024-03-26 10:32:28 -0700

    merging conflicts

--- Game3/game3.js
+++ Game3/game3.js
@@ -1,4 +1,5 @@
-
+let exercise3x = 200;
+let exercise3y = 200;
 
 function game3Preload(){
   
@@ -18,21 +19,33 @@ function game3Setup(){
   stroke(0);
   fill("pink");
   rect(0,0, 200);
-  fill("blue");
+  fill("plum");
   rect(200,0, 200,200);
   fill("lightblue");
   rect(200,200, 200);
-  fill("yellow");
+  fill("lightyellow");
   rect(0,200, 200,200);
+  fill("deeppink");
+  circle(100,300, 100);
+  fill("lightgreen");
+  rect(50,60,100,100);
+  fill("steelblue");
+  triangle(350, 150, 250, 150, 300,50);
+  fill("coral");
+  rect(230,250,150,100);
+  
   noStroke();
+
+  
 }
 
 function game3Draw(){
   
-
+  exercise3x = exercise3x + random(4) - 2;
+  exercise3y = exercise3y + random(4) - 2;
   
   fill(220);
   circle(exercise3x, exercise3y, 10);
 
-  text("carsyn", 100, 100);
+  
 }
