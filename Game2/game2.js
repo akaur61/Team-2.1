725aafce9559f2e3fd1629f50321e7d5fe991391
akaur61 <akaur61@asu.edu>
2024-04-04 11:36:03 -0700

    blah
    blah

--- Game2/game2.js
+++ Game2/game2.js
@@ -1,11 +1,12 @@
 let game2Photo1;
-
+//hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
 function game2Preload(){
   game2Photo1= loadImage("Game2/penguin.png");
 }
 
 function game2Setup(){
-  document.body.style.backgroundColor = "blue";
+ // document.body.style.backgroundColor = "blue";
+  background(178,255,255);
   // background('rgb(178,255,255)');
   currentActivity = 2;
   
@@ -18,12 +19,29 @@ function game2Setup(){
 }
 
 function game2Draw(){
-  background('rgb(178,255,255)');
+  //background(178,255,255);
   image(game2Photo1,50,140);
-  
+
+ 
+  if(mouseIsPressed) {
+    fill(0);
+  } else {
+    noStroke();
+    noFill();
+  }
+  ellipse(mouseX, mouseY, 5, 5);
 }
 
+// function draw() {
+//   if (mouseIsPressed) {
+//     fill(255,255,255);
+//   } else {
+    
+//     fill(28,89)
+//   }
+//   ellipse(mouseX, mouseY, 80, 80);
+// }
+
 function game2MousePressed(){
-  
   
 }
